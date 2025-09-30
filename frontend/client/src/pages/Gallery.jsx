137b9106ec10e72730ea5c46f955fb86c3db629a
“Nick” <nicholaschihanga@gmail.com>
2025-09-30 11:51:18 +0200

    Aded Gallery backend and image API

--- frontend/client/src/pages/Gallery.jsx
+++ frontend/client/src/pages/Gallery.jsx
@@ -1,37 +1,36 @@
-import React from "react";
-import "./Gallery.css"; // Create this CSS file for styles
+import React, { useEffect, useState } from "react";
+import "./Gallery.css";
 
-const images = [
-  "/images/img1.jpg",
-  "/images/img2.jpg",
-  "/images/img3.jpg",
-  "/images/img4.jpg",
-  "/images/img5.jpg",
-  "/images/img6.jpg",
-  "/images/img7.jpg",
-  "/images/img8.jpg",
-  "/images/img9.jpg",
-];
+const Gallery = () => {
+  const [images, setImages] = useState([]);
 
-const Gallery = () => (
-  <div className="gallery-page">
-    <header className="gallery-header">
-      <img src="/logo.png" alt="Logo" className="gallery-logo" />
-      <h1>GALLERY</h1>
-      <div className="gallery-nav">
-        <span>BOOKINGS</span>
-        <span>LOGIN</span>
-        <span className="gallery-toggle"></span>
-      </div>
-    </header>
-    <div className="gallery-grid">
-      {images.map((src, idx) => (
-        <div className="gallery-item" key={idx}>
-          <img src={src} alt={`Gallery ${idx + 1}`} />
+  useEffect(() => {
+    fetch("http://localhost:8000/backend/get-images.php")
+      .then((res) => res.json())
+      .then((data) => setImages(data))
+      .catch(() => setImages([]));
+  }, []);
+
+  return (
+    <div className="gallery-page">
+      <header className="gallery-header">
+        <img src="/logo.png" alt="Logo" className="gallery-logo" />
+        <h1>GALLERY</h1>
+        <div className="gallery-nav">
+          <span>BOOKINGS</span>
+          <span>LOGIN</span>
+          <span className="gallery-toggle"></span>
         </div>
-      ))}
+      </header>
+      <div className="gallery-grid">
+        {images.map((src, idx) => (
+          <div className="gallery-item" key={idx}>
+            <img src={src} alt={`Gallery ${idx + 1}`} />
+          </div>
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Gallery;
