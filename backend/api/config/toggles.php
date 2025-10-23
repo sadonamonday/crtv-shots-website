957c1d8fbde54d0204b8fadb10dc695130e6f7d5
“Nick” <nicholaschihanga@gmail.com>
2025-10-23 13:08:54 +0200

    initial commit

--- backend/api/config/toggles.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Global feature toggles for backend behavior
-// Set to true to temporarily bypass 2FA during development/testing.
-// IMPORTANT: Do NOT enable in production.
-if (!defined('DISABLE_2FA')) {
-    define('DISABLE_2FA', true);
-}
